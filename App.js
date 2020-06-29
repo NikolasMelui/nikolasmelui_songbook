cbc682359cc2e8640bc4d0843ed056c7a7712ca2
nikolasmelui <slak@samaradom.ru>
2020-06-29 19:02:52 +0400

    Add new App.js logic

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 import Header from "./components/Header";
+import ListSongs from "./components/ListSongs";
 
 const App = () => {
+
+  const [songs, setSong] = useState([
+    {
+      title: "Hello, Dolly",
+      text: `Oh, hello Dolly, well, hello Dolly
+It's so nice to have you back where you belon
+You're lookin' swell, Dolly, I can tell, Dolly
+You're still glowin', you're still crowin', you're still goin' strong
+We feel the room swayin' while the band's playin'
+One of your old favorite songs from way back when
+So, take her wrap fellas, find her an empty lap, fellas
+Dolly won't never ever go away again
+Hey, hello Dolly, well, hello Dolly
+It's really nice to have you back where you belong
+Girl, you're lookin' swell, Dolly, I can tell, Dolly
+Still glowin', still crowin', you're still goin' strong…`
+    }
+  ])
+
   return (
     <View style={styles.container}>
-      <Header />
-      <Text style={styles.text}>Hello, Dolly!</Text>
+      <Header title="SongBook" />
+      <FlatList data={songs} renderItem={({ item }) => <ListSongs song={item} />} keyExtractor={(song, index) => index.toString()} />
     </View >)
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,13 +38,6 @@ const styles = StyleSheet.create({
     width: '100%',
     paddingTop: 60
   },
-  text: {
-    color: 'darkslateblue',
-    display: 'flex',
-    paddingTop: 300,
-    fontSize: 30,
-    textAlign: 'center'
-  }
 })
 
 export default App;
