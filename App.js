9934232d3effc294caa4bc02317bfcec72cf1c54
nikolasmelui <slak@samaradom.ru>
2020-06-29 16:19:46 +0400

    Add Header and main text to the app

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import Header from "./components/Header";
+
 const App = () => {
-  return (<View style={styles.container}>
-    <Text style={styles.text}>
-      Hello, Dolly
-    </Text>
-  </View >)
+  return (
+    <View style={styles.container}>
+      <Header />
+      <Text style={styles.text}>Hello, Dolly!</Text>
+    </View >)
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, justifyContent: 'center', alignItems: 'center'
-  }, text: {
-    fontSize: 30
+    flex: 1,
+    width: '100%',
+    paddingTop: 60
+  },
+  text: {
+    color: 'darkslateblue',
+    display: 'flex',
+    paddingTop: 300,
+    fontSize: 30,
+    textAlign: 'center'
   }
 })
 
