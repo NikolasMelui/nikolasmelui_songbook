3651e4b8b7f594c7273a6e901dbe39faa2afb940
nikolasmelui <slak@samaradom.ru>
2020-06-30 15:19:40 +0400

    Create the mainStack file for the router stack

---
+++ routes/mainStack.js
@@ -0,0 +1,32 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from "../screens/HomeScreen";
+import SongScreen from "../screens/SongScreen";
+
+const Stack = createStackNavigator()
+
+import songsList from "../songsList";
+
+const MainStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" options={{
+          headerStyle: {
+            backgroundColor: 'darkslateblue',
+          },
+          headerTintColor: '#fff',
+        }} component={HomeScreen} />
+        {songsList.map((song, index) => (
+          <Stack.Screen name={song.title} component={SongScreen} key={index} />
+        ))}
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default MainStack;
