b34715868eb4c6d4df510fefda3fc67a1ed605a8
nikolasmelui <slak@samaradom.ru>
2020-06-30 15:18:22 +0400

    Add songs to the songsLIst

---
+++ songsList/index.js
@@ -0,0 +1,19 @@
+const songsList = [
+  {
+    title: "Hello, Dolly",
+    text: `Oh, hello Dolly, well, hello Dolly
+It's so nice to have you back where you belon
+You're lookin' swell, Dolly, I can tell, Dolly
+You're still glowin', you're still crowin', you're still goin' strong
+We feel the room swayin' while the band's playin'
+One of your old favorite songs from way back when
+So, take her wrap fellas, find her an empty lap, fellas
+Dolly won't never ever go away again
+Hey, hello Dolly, well, hello Dolly
+It's really nice to have you back where you belong
+Girl, you're lookin' swell, Dolly, I can tell, Dolly
+Still glowin', still crowin', you're still goin' strong…`
+  }
+];
+
+export default songsList;
